3ff7cd04c806dcfe0b36818f2466885fb2794a54
Joey Muething <jmuething@luc.id>
2016-12-15 14:17:59 -0600

    Added Object

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+
+var thing;
+var pretty = require('node_modeules/js-object-pretty-print');
+
+
 Realeyesit.EnvironmentDetector.detect()
-    .then(function (result) {
-        console.log(result)
-    });
+      .then(appendToDOM);
+
+function appendToDOM(response){
+  $(document).ready(function(){
+    thing = response;
+    if(thing.webcams.length > 0){
+      $("body").append("<p>Ok, we've detected you have a webcam</p>" +
+        "<p>" + thing.webcams.length + " unique webcam type(s) detected.</p>" +
+        "<p>Here's what else we found:</p>" +
+        "<p>" + pretty(thing) + "</p>");
+    }
+  });
+}
