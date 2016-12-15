1739eef45f5092736882cfaa9eef9a0961b3e8fe
Joey Muething <jmuething@luc.id>
2016-12-15 14:57:47 -0600

    Else statement

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ function appendToDOM(response){
       $("body").append("<p>Ok, we've detected you have a webcam</p>" +
         "<p>" + thing.webcams.length + " unique webcam type(s) detected.</p>" +
         "<p>Here's what else we found:</p>" +
-        "<p>" + JSON.stringify(thing) + "</p>");
+        "<p>" + JSON.stringify(thing) + "</p>");}
+
+
+    else {
+        $("body").append("<p>No webcam detected. Too Bad</p>");
     }
   });
 }
